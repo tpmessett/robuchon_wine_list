85aa103bb221de72cd69a528cac7516e2cb3826f
thomas messett <thomas.messett@stampapp.me>
2021-05-04 00:04:30 +0100

    sorted editing page to not allow name change to avoid slug issues with QR code

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,3 +1,4 @@
+
 class WinesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :show ]
 
@@ -13,8 +14,6 @@ class WinesController < ApplicationController
 
   def show
     @wine = Wine.friendly.find(params[:id])
-    @qr_code = RQRCode::QRCode.new(request.original_url)
-    @svg = @qr_code.as_svg( shape_rendering: 'crispEdges', standalone: true, module_size: 6 )
   end
 
   def new
@@ -25,6 +24,21 @@ class WinesController < ApplicationController
     @wine = Wine.new(wine_params)
 
     if @wine.save
+      qr_code = RQRCode::QRCode.new(URI.parse(request.original_url).host + "/wines/" + @wine.friendly_id)
+      png = qr_code.as_png(
+        bit_depth: 1,
+        border_modules: 4,
+        color_mode: ChunkyPNG::COLOR_GRAYSCALE,
+        color: 'black',
+        file: nil,
+        fill: 'white',
+        module_px_size: 6,
+        resize_exactly_to: false,
+        resize_gte_to: false,
+        size: 300
+      )
+      @wine.qr_code = Base64.encode64(png.to_s)
+      @wine.save
       redirect_to wine_path(@wine)
     else
 
